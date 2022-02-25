21a403fd71cac1b645b2f15ebefb17779eb2b9f6
Muhammad Farid Ullah <faridkhan125janan@gmail.com>
2022-02-25 05:21:14 +0500

    Initial Commit

---
+++ MyDiary/MyDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDiaryApp.swift
+//  MyDiary
+//
+//  Created by Muhammad Farid Ullah on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
